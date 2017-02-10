47352708ed184090c95d7f1cf9b0490eb981c630
Mikhail Komarov <nemo1369@gmail.com>
2017-02-11 00:44:51 +0300

    Valid Boost.Test dynamic linking implemented

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,11 @@
+#ifdef BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE chainbase test
-
 #include <boost/test/unit_test.hpp>
+#else
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+#endif
+
 #include <chainbase/chainbase.hpp>
 
 #include <boost/multi_index_container.hpp>
