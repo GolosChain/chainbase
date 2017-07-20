126f31319f629d0873cfdbff2b147f0c569970f2
Mikhail Komarov <nemo1369@gmail.com>
2017-07-20 03:56:58 +0300

    Test naming updated

--- test/test.cpp
+++ test/test.cpp
@@ -133,7 +133,7 @@ BOOST_AUTO_TEST_CASE(open_and_create) {
         BOOST_REQUIRE_EQUAL(new_book.a, copy_new_book.a);
         BOOST_REQUIRE_EQUAL(new_book.b, copy_new_book.b);
     } catch (...) {
-        bfs::remove_all(temp);
+        boost::filesystem::remove_all(temp);
         throw;
     }
 }
